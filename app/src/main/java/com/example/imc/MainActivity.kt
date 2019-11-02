e77dda09fa37a8ebb785b8c80ca81df1022a89fd
Victor Dantas <victor.brejo@gmail.com>
2019-11-02 08:16:15 -0300

    att

--- app/src/main/java/com/example/imc/MainActivity.kt
+++ app/src/main/java/com/example/imc/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.imc
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,51 +13,15 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var altura: EditText
-    private lateinit var peso: EditText
-    private lateinit var idade: EditText
-    private lateinit var botao: Button
-    private lateinit var texto: TextView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        altura = findViewById(R.id.etAltura)
-        peso = findViewById(R.id.etPeso)
-        idade = findViewById(R.id.etIdade)
-        botao = findViewById(R.id.btCalcularID)
-        texto = findViewById(R.id.tvTextoID)
 
-
-        this.botao.setOnClickListener({calcIMC(it)})
 
     }
 
-     fun calcIMC(textView: View?) {
-        val pesoIMC = peso.toString().toFloat()
-
-        val alturaIMC = (altura.toString().toFloat() * altura.toString().toFloat())
-
-        val imc = pesoIMC / alturaIMC
-
-        if(imc <= 18.5) {
-            this.texto.text = "Abaixo do Peso"
-        }
-        else if(imc <= 24.9) {
-            this.texto.text = "Peso Ideal"
-
-        }
-        else if(imc <= 29.9) {
-            this.texto.text = "Levemente Acima do Peso"
-
-        }
-        else if(imc <= 34.9) {
-            this.texto.text = "Obesidade"
-
-        }
-    }
 
 }
 
