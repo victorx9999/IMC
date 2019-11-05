ed8061258398b53fb5ed8803c8235829a842ae87
aleffkol <kluivertnew2013@gmail.com>
2019-11-05 18:22:18 -0300

    Add Banco and PessoaDao

--- app/src/main/java/com/example/imc/Pessoa.kt
+++ app/src/main/java/com/example/imc/Pessoa.kt
@@ -3,6 +3,7 @@ package com.example.imc
 import java.io.Serializable
 
 class Pessoa : Serializable {
+    var id: Int
     var nome: String
     var idade: Int
     var peso: Double
@@ -10,6 +11,15 @@ class Pessoa : Serializable {
 
 
     constructor(nome: String, idade: Int, peso: Double, altura: Int){
+        this.id = -1
+        this.nome = nome
+        this.idade = idade
+        this.peso = peso
+        this.altura = altura
+    }
+
+    constructor(id: Int, nome: String, idade: Int, peso: Double, altura: Int){
+        this.id = id
         this.nome = nome
         this.idade = idade
         this.peso = peso
---
+++ app/src/main/java/com/example/imc/PessoaDAO.kt
@@ -0,0 +1,57 @@
+package com.example.imc
+
+import android.content.ContentValues
+import android.content.Context
+// nome: String, idade: Int, peso: Double, altura: Int
+class PessoaDAO {
+    var bancoHelper: BancoHelper
+
+    constructor(context: Context){
+        this.bancoHelper = BancoHelper(context)
+    }
+
+    fun insert(pessoa: Pessoa){
+        val banco = this.bancoHelper.writableDatabase
+        val cv = ContentValues()
+        cv.put("nome", pessoa.nome)
+        cv.put("idade", pessoa.idade)
+        cv.put("peso", pessoa.peso)
+        cv.put("altura", pessoa.altura)
+        banco.insert("pessoas", null, cv)
+    }
+
+    fun get(id: Int): Pessoa?{
+        return null
+    }
+
+    fun get(): ArrayList<Pessoa>{
+        val lista = arrayListOf<Pessoa>()
+        val banco = this.bancoHelper.readableDatabase
+        val colunas = arrayOf("id", "nome", "idade", "peso", "altura")
+
+        val c = banco.query("pessoas", colunas, null, null, null, null, null)
+
+        c.moveToFirst()
+
+        do{
+            val id = c.getInt(c.getColumnIndex("id"))
+            val nome = c.getString(c.getColumnIndex("nome"))
+            val idade = c.getInt(c.getColumnIndex("idade"))
+            val peso = c.getDouble(c.getColumnIndex("peso"))
+            val altura = c.getInt(c.getColumnIndex("altura"))
+
+            val pessoa = Pessoa(id, nome, idade, peso, altura)
+            lista.add(pessoa)
+        }while (c.moveToNext())
+
+        return lista
+    }
+
+    fun update(id: Int, pessoa: Pessoa){
+
+    }
+
+    fun delete(id: Int){
+
+    }
+}
