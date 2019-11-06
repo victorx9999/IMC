fc81f6893ebf4b32c3cc454e49a16dd4e8d2410b
aleffkol <kluivertnew2013@gmail.com>
2019-11-05 22:52:27 -0300

    Layout, Adapter e DAO de pessoa

--- app/src/main/java/com/example/imc/PessoaDAO.kt
+++ app/src/main/java/com/example/imc/PessoaDAO.kt
@@ -48,7 +48,14 @@ class PessoaDAO {
     }
 
     fun update(id: Int, pessoa: Pessoa){
-
+        val cv = ContentValues()
+        val where = "id = ?"
+        val wherep = arrayOf(pessoa.id.toString())
+        cv.put("nome", pessoa.nome)
+        cv.put("idade", pessoa.idade)
+        cv.put("peso", pessoa.peso)
+        cv.put("altura", pessoa.altura)
+        this.bancoHelper.writableDatabase.update("pessoas", cv, where, wherep)
     }
 
     fun delete(id: Int){
@@ -56,4 +63,32 @@ class PessoaDAO {
         var whereP = arrayOf(id.toString())
         this.bancoHelper.writableDatabase.delete("pessoas", where, whereP)
     }
+
+    fun select(): List<Pessoa>{
+        val lista = arrayListOf<Pessoa>()
+        val banco = this.bancoHelper.readableDatabase
+        val colunas = arrayOf("id", "nome", "idade")
+        val c = banco.query("pessoas", colunas, null, null, null, null,null)
+
+        c.moveToFirst()
+        do {
+            var id = c.getInt(c.getColumnIndex("id"))
+            var nome = c.getString(c.getColumnIndex("nome"))
+            var idade = c.getInt(c.getColumnIndex("idade"))
+            var peso = c.getDouble(c.getColumnIndex("peso"))
+            var altura = c.getInt(c.getColumnIndex("altura"))
+
+            lista.add(Pessoa(id, nome, idade, peso, altura))
+        }while(c.moveToNext())
+
+        return lista
+    }
+
+    fun count(): Int{
+        val colunas = arrayOf("id")
+        val banco = this.bancoHelper.readableDatabase
+        val c = banco.query("pessoas", colunas, null, null, null, null, null)
+
+        return c.count
+}
 }
